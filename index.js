b649ab3b93159ad6e6aa5332dccd10fb72eb571d
Pepsi <git@pshar.ma>
2022-12-04 17:08:54 +1100

    v0.10.1 code comments for index.js

--- index.js
+++ index.js
@@ -5,10 +5,19 @@ const costPerMinute = (0.004 / (197/60)) + (96000000000/(26637*3650000000));
 document.querySelector(".query-button").addEventListener('click', () => {
     try{
         document.querySelector(".share").innerHTML = ""
+        // We can rely on the fact that JS will automatically exclude the word "minutes" from the time listening input (verified through testing on like 18 different devices).
+        // So we don't need to filter the input, we can just convert it to an integer.
         const timeListening = parseInt(document.querySelector(".query-input").value)
         const accountType = parseFloat(document.querySelector(".account-type").value)
         // the account type selector actually just returns the cost in USD for your account type lmao
+        // The values used for accountType are:
+        // 11.99 USD/month for Personal Plans
+        // 5.99 USD/month for Student Plans
+        // 15.99 USD/month divided by 2 for Duo plans
+        // 18.99 USD/month divided by 6 for Family plans
+        // I'm assuming family plans are getting maximum utilisation because I'm lazy.
         const netCost = ((timeListening * costPerMinute)-(accountType*12)).toFixed(2)
+        // This part can probably be done much more efficiently, I just did it like this because I don't know JS.
         let ResponseText;
         if (netCost > 0) {
             ResponseText = "Spotify lost $" + netCost + " from your subscription last year 🤑"
@@ -20,6 +29,7 @@ document.querySelector(".query-button").addEventListener('click', () => {
             ResponseText = "Spotify broke even with your subscription last year!"
         }
         document.querySelector(".response").textContent = ResponseText
+        // Change from second to first person so people can share to social media
         socialResponseText = ResponseText.replace("your", "my")
         document.querySelector(".share-twitter").innerHTML = `<a href="https://twitter.com/share?ref_src=twsrc%5Etfw?related=spotifywrapped&text=${encodeURIComponent(socialResponseText)}&url=${encodeURIComponent("https://spotiprofit.xyz/")}" class="twitter-share-button"><i class="fab fa-twitter"></i> Share on Twitter</a>`
         document.querySelector(".share-reddit").innerHTML = `<a href="https://reddit.com/submit?url=https://spotiprofit.xyz&title=${encodeURIComponent(socialResponseText)}" class="reddit-share-button"><i class="fab fa-reddit"></i> Share on Reddit</a>`
